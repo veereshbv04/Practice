c9054723403947df7c83ab25e76e847a7be5b3f4
Veeresh B V <Veereshbv04@gmail.com>
2021-08-16 22:40:19 +0530

    question 1 completed

---
+++ sample_q1/app.js
@@ -0,0 +1,12 @@
+const firstNum =document.querySelector("#first-input");
+const secondNum  = document.querySelector("#second-input");
+const buttons = document.querySelectorAll(".operation");
+const outputDiv = document.querySelector("#output-div");
+
+
+
+buttons[0].addEventListener("click", ()=>{
+    var n1 = Number(firstNum.value);
+    var n2 = Number(secondNum.value);
+    outputDiv.innerText = n1 + n2
+})
