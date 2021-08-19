0496431e3e7e56dece5aec47a3799893e0137867
Veeresh B V <Veereshbv04@gmail.com>
2021-08-19 20:42:55 +0530

    d is added to translate in URL to get error 404 and then played around it

--- api_call/app.js
+++ api_call/app.js
@@ -2,38 +2,46 @@ const inputText = document.querySelector("#input-text");
 const btn = document.querySelector("#btn");
 const outputDiv = document.querySelector("#output-div");
 
-var serverURL = "https://ap.funtranslations.com/translate/navi.json";
+var serverURL = "https://api.funtranslations.com/translated/navi.json";
 
 function constructURL(text){
     return serverURL + "?" + "text=" +text ;
 }
 
-
 function errorHandler(error){
     
     console.log("In error");
     console.log(error);
+ 
 }
 
 function clickHandler(){
     
-    console.log("hh");
+    console.log("Button Clicked");
     var userText = inputText.value;
     fetch(constructURL(userText))
     .then(function parse(response){
 
         if(response.status === 200){
-            console.log("Resonse status this is");
+            console.log("Response status code is 200");
         }
+        console.log(response.status)
 
+        console.log(response.url);
         console.log(response);
         return response.json()
     })
     .then(json =>{
         console.log(json);
-  
-        var textranslated = json.contents.translated;
-        outputDiv.innerText = textranslated.toUpperCase();
+        console.log(json.error)
+        console.log(json.error.code);
+        if(json.error.code === 404){
+            outputDiv.innerText = json.error.message;
+        }
+        
+        // var textranslated = json.contents.translatedd;
+        // console.log(textranslated);
+        // outputDiv.innerText = textranslated.toUpperCase();
     })
     .catch(errorHandler)
 }
