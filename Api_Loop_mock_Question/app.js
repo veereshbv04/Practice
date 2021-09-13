32e9a6ea6485fadca7bf842adb21ffdaf3b07437
Veeresh B V <Veereshbv04@gmail.com>
2021-09-13 22:24:43 +0530

    API Mock Question is done

---
+++ Api_Loop_mock_Question/app.js
@@ -0,0 +1,24 @@
+var url = "https://jsonplaceholder.typicode.com/todos";
+
+const outPutDiv = document.querySelector("div");
+
+
+fetch(url)
+.then((response) =>{
+    console.log(response);
+    
+    return response.json()
+})
+.then((json) =>{
+    console.log(typeof json);
+    json.map((el)=>{
+        if(!el.completed){
+            console.log(el);
+            outPutDiv.innerHTML +=  `<p class="not-true">${el.title} ${el.id} <br> <\p>`
+        }else{
+            console.log(el);
+            outPutDiv.innerHTML +=  `<p>${el.title} ${el.id} <br> <\p>`
+        }
+        outPutDiv.innerHTML += `${el.completed}`
+    })
+})
