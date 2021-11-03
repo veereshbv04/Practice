7d1fa6141827eab340e16ec3102b4d70b83bbd9f
Veeresh B V <Veereshbv04@gmail.com>
2021-11-03 23:18:14 +0530

    Name and Praise

--- Live Practice/app.js
+++ Live Practice/app.js
@@ -1,14 +1,14 @@
-const name = document.querySelector("#name");
-const password = document.querySelector("#password");
+const userInput = document.querySelector("input");
 const outputDiv = document.querySelector("div");
 
-password.onkeyup = ()=>{
-    var uname = name.value ;
-    var upassword = password.value
-
-    if(upassword.includes(uname)){
-        outputDiv.innerText="Password cannot contain name"
-    }else{
-        outputDiv.innerText="Password id is in correct format"
+userInput.onkeyup = ()=>{
+    if(userInput.value ==="Tanvi"){
+        outputDiv.innerText="She is the best CEO ever!"
+    }else if(userInput.value === "Tanay"){
+        outputDiv.innerText = "He is our FUNNY mentor!"
+    }else if( userInput.value === "Swapnil"){
+        outputDiv.innerText = "He helps us CONQUER jobs!";
+    }else if(userInput.value === "Akanksha"){
+        outputDiv.innerText = "She is our CHEERFUL Interviewer!"
     }
 }
