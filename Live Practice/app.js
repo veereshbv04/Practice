e08b1da6cfbd48d98d0050dbe6d76c34e7853eab
Veeresh B V <Veereshbv04@gmail.com>
2021-11-03 22:51:52 +0530

    APi 404 and 401 error sample question

--- Live Practice/app.js
+++ Live Practice/app.js
@@ -1,10 +1,30 @@
-const userText = document.querySelector("input");
-const btn = document.querySelectorAll("button");
-const outputDiv = document.querySelector("div");
 
-btn[0].addEventListener("click", ()=>{
-    var text = userText.value
-    outputDiv.innerText=text
-    userText.style.color="blue"
-    outputDiv.style.color="blue"
+const btn = document.querySelector("button");
+
+// var el = document.createElement("div");
+// var text = document.createTextNode("GgGg");
+// el.appendChild(text)
+// btn.appendChild(el)
+
+var url ="https://mystery-api.kushanksriraj.repl.co/get";
+
+btn.addEventListener("click", ()=>{
+    var mtext ;
+    fetch(url)
+    .then(response =>{
+        if(response.status === 404){
+            mtext="page not found"
+        }else if(response.status === 401){
+            mtext = "you are not looed in"
+        }
+    })
+
+
+    var el = document.createElement("div");
+    var text = document.createTextNode(mtext);
+    el.appendChild(text)
+    document.body.appendChild(el)
+
+
 })
+
