077e4dd56ef6296db5daa0b9ac3a4ccaf126ec70
Veeresh B V <Veereshbv04@gmail.com>
2021-11-03 22:32:53 +0530

    input color change

--- Live Practice/app.js
+++ Live Practice/app.js
@@ -1,22 +1,10 @@
-const nameDiv = document.querySelector("div");
+const userText = document.querySelector("input");
 const btn = document.querySelectorAll("button");
-const apiOutput = document.querySelector("#apiOutput");
+const outputDiv = document.querySelector("div");
 
 btn[0].addEventListener("click", ()=>{
-    nameDiv.style.borderWidth = `5px`
-})
-
-
-btn[1].addEventListener("click", ()=>{
-    nameDiv.style.borderWidth = `10px`
-})
-
-var url = "https://quick-access-api.desaihetav.repl.co"
-
-btn[2].addEventListener("click", ()=>{
-    fetch(url)
-    .then(response => response.json())
-    .then(res =>{
-        apiOutput.innerHTML =`<p>${res.success} ${res.message}`
-    })
+    var text = userText.value
+    outputDiv.innerText=text
+    userText.style.color="blue"
+    outputDiv.style.color="blue"
 })
